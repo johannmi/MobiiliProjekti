1bf1aea90c178bc8fb2b90be5c5256850cf5e260
Jossu <johanna.miettinen2@metropolia.fi>
2019-04-23 13:34:14 +0300

    Projektin aloitus

---
+++ app/src/main/java/com/example/mobiiliprojekti/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mobiiliprojekti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+
+* TESTI TESTI
+*
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
